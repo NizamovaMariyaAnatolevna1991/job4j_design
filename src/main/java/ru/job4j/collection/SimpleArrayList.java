c4dd1aa1534d37632d456c1e07ea0b844ebdd239
masha <musha_skorpion@mail.ru>
2025-04-08 00:45:10 +0500

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,92 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+        this.size = 0;
+        this.modCount = 0;
+
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            grow();
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, size);
+        T oldValue = container[index];
+        container[index] = newValue;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        T removedValue = container[index];
+        System.arraycopy(
+                container,
+                index + 1,
+                container,
+                index,
+                size - index - 1
+        );
+        modCount++;
+        size--;
+        return removedValue;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int cursor = 0;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException("Collection was modified");
+                }
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("No more elements");
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException("Collection was modified");
+                }
+                return container[cursor++];
+            }
+        };
+    }
+
+    private void grow() {
+        int newCapacity = container.length == 0 ? 1 : container.length * 2;
+        container = Arrays.copyOf(container, newCapacity);
+    }
+}
