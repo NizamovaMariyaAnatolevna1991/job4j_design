b9f44201faa2edf917aad07a8024c18c06150517
masha <musha_skorpion@mail.ru>
2025-05-30 13:22:31 +0500

    2. Что такое Socket? [#4850]

---
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,54 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream output = socket.getOutputStream();
+                     BufferedReader input = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String requestLine = input.readLine();
+                    System.out.println("Получен запрос: " + requestLine);
+
+                    String msg = extractParameter(requestLine, "msg");
+
+                    if ("Bye".equals(msg)) {
+                        output.write("HTTP/1.1 200 OK\r\n\r\nServer is shutting down...".getBytes());
+                        output.flush();
+                        System.out.println("Получен сигнал завершения работы. Сервер останавливается...");
+                        server.close();
+                        break;
+                    }
+
+                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
+                        System.out.println(string);
+                    }
+                    output.flush();
+                }
+            }
+        }
+    }
+
+    private static String extractParameter(String requestLine, String paramName) {
+        if (requestLine == null || !requestLine.contains("?")) {
+            return null;
+        }
+
+        String queryString = requestLine.split("\\?")[1].split(" ")[0];
+
+        String[] params = queryString.split("&");
+        for (String param : params) {
+            String[] keyValue = param.split("=");
+            if (keyValue.length == 2 && keyValue[0].equals(paramName)) {
+                return keyValue[1];
+            }
+        }
+        return null;
+    }
+}
