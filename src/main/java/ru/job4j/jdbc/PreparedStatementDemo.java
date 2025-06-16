dc71fb1d9d99f37b2083719b44e9c501bb847cda
masha <musha_skorpion@mail.ru>
2025-06-16 18:57:52 +0500

    0.2. PreparedStatement [#379307]

---
+++ src/main/java/ru/job4j/jdbc/PreparedStatementDemo.java
@@ -0,0 +1,96 @@
+package ru.job4j.jdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class PreparedStatementDemo {
+
+    private Connection connection;
+
+    public PreparedStatementDemo() throws Exception {
+        initConnection();
+    }
+
+    public void initConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        String login = "postgres";
+        String password = "password";
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public void insert(City city) {
+        try (PreparedStatement statement =
+                     connection.prepareStatement("INSERT INTO cities(name, population) VALUES (?, ?)",
+                             Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.execute();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    city.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean update(City city) {
+        boolean result = false;
+        try (PreparedStatement statement =
+                     connection.prepareStatement("UPDATE cities SET name = ?, population = ? WHERE id = ?")) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.setInt(3, city.getId());
+            result = statement.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public void delete(int id) {
+        try (PreparedStatement statement =
+                     connection.prepareStatement("DELETE FROM cities WHERE id = ?")) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<City> findAll() {
+        List<City> cities = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM cities")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    cities.add(new City(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getInt("population")
+                    ));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cities;
+    }
+
+    public static void main(String[] args) throws Exception {
+        PreparedStatementDemo preparedStatementDemo = new PreparedStatementDemo();
+        preparedStatementDemo.initConnection();
+        preparedStatementDemo.insert(new City(2,"Moskow", 2000000));
+        preparedStatementDemo.update(new City(2,"Rostov", 2000000));
+        System.out.println(preparedStatementDemo.update(new City(2,"Rostov", 2000000)));
+        preparedStatementDemo.delete(2);
+        List<City> cities = preparedStatementDemo.findAll();
+        Iterator<City> iterator = cities.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
