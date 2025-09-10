25940ed3278b86e909ffb2374134d856477f090b
masha <musha_skorpion@mail.ru>
2025-09-10 15:17:33 +0500

    1. Хранилище продуктов [#852]

--- src/main/java/ru/job4j/ood/foodservice/store/Shop.java
+++ src/main/java/ru/job4j/ood/foodservice/store/Shop.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 public class Shop extends AbstractStore {
     @Override
     public boolean canAccept(Food food, LocalDateTime currentDate) {
-        double freshness = getFreshness(food,currentDate);
+        double freshness = getFreshness(food, currentDate);
         boolean inRange = freshness >= MIN_FRESHNESS_FOR_SALE && freshness <= FRESH_THRESHOLD;
 
         if (inRange && freshness < DISCOUNT_THRESHOLD) {
